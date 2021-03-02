68e41370f2b92d6385003e540ef6edb14962361d
Philipp Kitzberger <philipp@kitze.net>
2021-03-02 09:17:11 +0100

    Bumped version

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
 	'title' => 'Dragon Drop',
-	'description' => '',
+	'description' => 'ViewHelper to add \'copy-paste\' to EXT:mask containers in the page module',
 	'category' => 'system',
 	'state' => 'stable',
 	'uploadfolder' => 0,
@@ -10,10 +10,10 @@ $EM_CONF[$_EXTKEY] = [
 	'author' => 'Philipp Kitzberger',
 	'author_email' => 'typo3@kitze.net',
 	'author_company' => '',
-	'version' => '1.0.0',
+	'version' => '1.0.1',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '8.7.0-',
+			'typo3' => '8.7.0-10.4.99',
 		],
 		'conflicts' => [],
 		'suggests' => [],
