39f1b57c698c810921004e7f08b6a816caad304d
Nikita Hovratov <nikita.h@live.de>
2021-03-03 19:15:19 +0100

    Remove unused use statement

--- Classes/Hooks/DataHandlerHook.php
+++ Classes/Hooks/DataHandlerHook.php
@@ -5,7 +5,6 @@ namespace Kitzberger\DragonDrop\Hooks;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
 use TYPO3\CMS\Core\Database\ConnectionPool;
-use TYPO3\CMS\Core\Database\Connection;
 use TYPO3\CMS\Core\Database\Query\Restriction\DeletedRestriction;
 
 class DataHandlerHook
